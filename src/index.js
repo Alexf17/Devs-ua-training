a65085f76f6b48dc684ca629af0d4715d9888e99
Oleksii <achili3f17@gmail.com>
2022-09-12 22:20:23 +0300

    search_by_name

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 import PictureService from './js/api'
-
-
 const pictureService = new PictureService()
 
-async function search() {
-    const responspictureService = await pictureService.fetchGallery()
-    console.log(responspictureService)
+const formEl = document.querySelector("form")
+const galleryWrap = document.querySelector(".render")
+formEl.addEventListener('submit', onSubmit)
+
+async function onSubmit(e) {
+    e.preventDefault()
+    pictureService.name = e.currentTarget.elements.query.value.trim().toLowerCase()
+    const {data} = await pictureService.fetchByName()
+    markUp(data.drinks)
 }
 
-console.log(search())
-
+function markUp(card) {
+    galleryWrap.insertAdjacentHTML('beforeend', card.map(
+        ({
+    strDrinkThumb,strDrink,strIngredient1,strIngredient2,strIngredient3,strIngredient4,strGlass,
+        }) =>
+     `<div class="photo-card">
+            <img src="${strDrinkThumb}" alt="${strDrink}" loading="lazy" height="300" width="400" />
+            <div class="info">
+                <ul>
+                <li>${strIngredient1}</li>
+                <li>${strIngredient2}</li>
+                <li>${strIngredient3}</li>
+                </ul>
+                <p class="info-item">
+                <b>Type of Glass: ${strGlass}</b>
+                </p>
+            </div>
+            </div>`
+    )
+        .join('')
+    )
+}
--- src/js/api.js
+++ src/js/api.js
@@ -2,16 +2,42 @@ import axios from 'axios'
 
 export default class PictureService {
   constructor() {
-    this.searchQuery = "Alcoholic"
+    this.name = ""
+    this.letter = ""
+    this.id = 0
+    
+          
   }
-  async fetchGallery() {
+  async fetchById() {
     axios.defaults.baseURL = 'https://www.thecocktaildb.com/api/json/v1/1'
       try {
-        const response = await axios.get(`filter.php?a=${this.searchQuery}`)
+        const response = await axios.get(`lookup.php?i=${this.id}`)
           return response
           console.log(response)
       } catch (error) {
         console.log(error)
     }
   }
+  async fetchByName() {
+    axios.defaults.baseURL = 'https://www.thecocktaildb.com/api/json/v1/1'
+      try {
+        const response = await axios.get(`search.php?s=${this.name}`)
+          return response
+          console.log(response)
+      } catch (error) {
+        console.log(error)
+    }
+  }
+  async fetchByLetter() {
+    axios.defaults.baseURL = 'https://www.thecocktaildb.com/api/json/v1/1'
+      try {
+        const response = await axios.get(`search.php?f=${this.letter}`)
+          return response
+          console.log(response)
+      } catch (error) {
+        console.log(error)
+    }
+  }
+
+
 }
