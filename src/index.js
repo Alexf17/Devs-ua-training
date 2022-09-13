17ca87126c5f4444beb00d40ea28c779c64c1b39
Volodymyr Labunets <vplabunets@gmail.com>
2022-09-13 23:11:30 +0300

    new commit

--- src/index.js
+++ src/index.js
@@ -1,30 +1,68 @@
-import PictureService from './js/api'
-const pictureService = new PictureService()
+import PictureService from './js/api';
+const pictureService = new PictureService();
 
-const formEl = document.querySelector("form")
-const galleryWrap = document.querySelector(".render")
-formEl.addEventListener('submit', onSubmit)
-const list = document.querySelector(".ingredients")
+const formEl = document.querySelector('form');
+const galleryWrap = document.querySelector('.render');
+formEl.addEventListener('submit', onSubmit);
+const list = document.querySelector('.ingredients');
+let keys = [];
+let values = [];
+let filteredArray = [];
+const finalArray = [];
 
 async function onSubmit(e) {
-    e.preventDefault()
-    pictureService.name = e.currentTarget.elements.query.value.trim().toLowerCase()
-    const {data} = await pictureService.fetchByName()
-    markUp(data.drinks)
+  e.preventDefault();
+  pictureService.name = e.currentTarget.elements.query.value
+    .trim()
+    .toLowerCase();
+  const { data } = await pictureService.fetchByName();
+  markUp(data.drinks);
+  for (drink of data.drinks) {
+    const filteredArray2 = [];
+    let ingrObject = {};
+    // console.log(drink);
+    filteredArray = Object.keys(drink).filter(element =>
+      element.match(/strIngredient/g)
+    );
+    // console.log(filteredArray);
+    for (let i = 0; i < filteredArray.length; i += 1) {
+      //   console.log(filteredArray[i]);
+
+      if (drink[filteredArray[i]] !== null) {
+        filteredArray2.push(drink[filteredArray[i]]);
+        ingrObject[filteredArray[i]] = drink[filteredArray[i]];
+      }
+    }
+    finalArray.push(ingrObject);
+    console.log(finalArray);
+  }
 }
+function consolee() {
+  console.log(finalArray);
+}
+setTimeout(consolee, 5000);
 
+console.log(keys);
 function markUp(card) {
-    const neww = card.filter(value => {
-        return Object.values(value)
-        
-    } )
-    console.log(neww)
+  const neww = card.filter(value => {
+    return Object.values(value);
+  });
+  console.log(neww);
 
-    galleryWrap.insertAdjacentHTML('beforeend', card.map(
+  galleryWrap.insertAdjacentHTML(
+    'beforeend',
+    card
+      .map(
         ({
-    strDrinkThumb,strDrink,strIngredient1,strIngredient2,strIngredient3,strIngredient4,strGlass,
+          strDrinkThumb,
+          strDrink,
+          strIngredient1,
+          strIngredient2,
+          strIngredient3,
+          strIngredient4,
+          strGlass,
         }) =>
-     `<div class="photo-card">
+          `<div class="photo-card">
             <img src="${strDrinkThumb}" alt="${strDrink}" loading="lazy" height="300" width="400" />
             <div class="info">
                 <ul class="ingredients">
@@ -37,19 +75,18 @@ function markUp(card) {
                 </p>
             </div>
             </div>`
-    )
-        .join('')
-    )
-    // list.insertAdjacentHTML('beforeend', card.map(
-    //     {
-    // strIngredient1,strIngredient2,strIngredient3,strIngredient4,
-    //     }) =>
-    //  `
-    // <li>${strIngredient1}</li>
-    // <li>${strIngredient2}</li>
-    // <li>${strIngredient3}</li>
-    // `
-    // )
-    //     .join('')
-    
+      )
+      .join('')
+  );
+  // list.insertAdjacentHTML('beforeend', card.map(
+  //     {
+  // strIngredient1,strIngredient2,strIngredient3,strIngredient4,
+  //     }) =>
+  //  `
+  // <li>${strIngredient1}</li>
+  // <li>${strIngredient2}</li>
+  // <li>${strIngredient3}</li>
+  // `
+  // )
+  //     .join('')
 }
