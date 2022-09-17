51c3f88b80fa552148131db43169b9b85b6c2645
Alexf17 <98961066+Alexf17@users.noreply.github.com>
2022-09-17 15:30:56 +0000

    Deploying to gh-pages from @ Alexf17/Devs-ua-training@677584adb1e872adf7d80fa9edd2435e8d4f725e 🚀

---
+++ index.0de1885a.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".page"),n=document.querySelector(".switcher__svg"),s=document.querySelector("body");n.addEventListener("click",(function(t){e.classList.toggle("dark__theme"),n.innerHTML="",s.classList.contains("dark__theme")?n.insertAdjacentHTML("beforeend",'<use href="/Devs-ua-training/symbol-defs.df4646d6.svg#icon-sun"></use>'):n.insertAdjacentHTML("beforeend",'<use href="/Devs-ua-training/symbol-defs.df4646d6.svg#icon-moon"></use>')}))}();
+//# sourceMappingURL=index.0de1885a.js.map
--- index.51541229.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".page"),n=document.querySelector(".switcher__svg"),s=document.querySelector("body");n.addEventListener("click",(function(t){e.classList.toggle("dark__theme"),n.innerHTML="",s.classList.contains("dark__theme")?n.insertAdjacentHTML("beforeend",'<use href="/Devs-ua-training/symbol-defs.df4646d6.svg#icon-sun"></use>'):n.insertAdjacentHTML("beforeend",'<use href="./images/symbol-defs.svg#icon-moon"></use>')}))}();
-//# sourceMappingURL=index.51541229.js.map
