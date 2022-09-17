1439cd20575fa85725597a6483905661617e3d8a
Oleksii <achili3f17@gmail.com>
2022-09-17 17:38:32 +0300

    222

--- src/js/changeTheme.js
+++ src/js/changeTheme.js
@@ -1,8 +1,15 @@
 const page = document.querySelector('.page')
 const buttonChangeTheme = document.querySelector('.theme-switcher')
+const body = document.querySelector('body')
 
 buttonChangeTheme.addEventListener('click', changeTheme)
-function changeTheme() {
-    page.classList.toggle('light__theme')
-    page.classList.toggle('dark__theme')
-}
+function changeTheme(e) {
+  
+    page.classList.toggle('dark__theme');
+    
+    if (body.classList.contains('dark__theme')) {
+        e.path[0].innerHTML = '<use href="/symbol-defs.a8b2e413.svg#icon-sun"></use>'
+    } else {
+        e.path[0].innerHTML = '<use href="/symbol-defs.a8b2e413.svg#icon-moon"></use>'
+    }
+    
