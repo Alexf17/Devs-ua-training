3d505aad002b82bfb6dacf1052d00ffc9baa76e0
Volodymyr Labunets <vplabunets@gmail.com>
2022-09-17 23:29:02 +0300

    17 09 2022 23 28

--- src/js/api.js
+++ src/js/api.js
@@ -33,13 +33,13 @@ export default class PictureService {
       console.log(error);
     }
   }
-  // async fetchNonAlchogol() {
-  //   axios.defaults.baseURL = 'https://www.thecocktaildb.com/api/json/v1/1';
-  //   try {
-  //     const response = await axios.get('filter.php?a=Non_Alcoholic');
-  //     return response;
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
+  async fetchNonAlchogol() {
+    axios.defaults.baseURL = 'https://www.thecocktaildb.com/api/json/v1/1';
+    try {
+      const response = await axios.get('filter.php?a=Non_Alcoholic');
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
--- src/js/modal.js
+++ src/js/modal.js
@@ -1,23 +1,38 @@
-// import PictureService from './api';
-
+import PictureService from './api';
+import { baseMarkUpCreate } from './createMarkUp';
 const confirmButtonEl = document.querySelector('.modal__btn');
-const backdropnEl = document.querySelector('.backdrop');
+const backdropEl = document.querySelector('.backdrop');
 const bodyEl = document.querySelector('.page');
-// const nonAlcoholicFetch = new PictureService();
-
+const alph_container = document.querySelector('.card__list');
+const headerEl = document.querySelector('.header__container');
+const alphEl = document.querySelector('.alph');
+const xxx = new PictureService();
+import Notiflix from 'notiflix';
 confirmButtonEl.addEventListener('click', onButtonClick);
-backdropnEl.addEventListener('click', onBackdropClick);
+backdropEl.addEventListener('click', onBackdropClick);
+
 function onButtonClick(event) {
-  backdropnEl.classList.add('visually-hidden');
+  backdropEl.classList.add('visually-hidden');
   bodyEl.classList.remove('no-scroll');
 }
-// async function onBackdropClick(event) {
-//   backdropnEl.classList.add('visually-hidden');
-//   bodyEl.classList.remove('no-scroll');
-//   try {
-//     const data = await nonAlcoholicFetch.fetchNonAlchogol();
-//     console.log(data);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
+
+async function onBackdropClick(event) {
+  if (backdropEl !== event.target) {
+    return;
+  }
+  backdropEl.classList.add('visually-hidden');
+  bodyEl.classList.remove('no-scroll');
+  try {
+    const { data } = await xxx.fetchNonAlchogol();
+
+    const cocktails = data.drinks;
+    baseMarkUpCreate(cocktails, alph_container);
+    headerEl.classList.add('visually-hidden');
+    alphEl.classList.add('visually-hidden');
+    Notiflix.Notify.info('Only Non alchoholic cocktails for you', {
+      timeout: '4000',
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
