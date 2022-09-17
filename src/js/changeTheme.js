41a0a9d0a9dd65718791e95d6465b94ddc7c3d40
Oleksii <achili3f17@gmail.com>
2022-09-17 18:22:01 +0300

    123

--- src/js/changeTheme.js
+++ src/js/changeTheme.js
@@ -4,14 +4,14 @@ const body = document.querySelector('body')
 
 buttonChangeTheme.addEventListener('click', changeTheme)
 function changeTheme(e) {
-    
-    page.classList.toggle('dark__theme');
-    buttonChangeTheme.innerHTML = '';
+
+    page.classList.toggle('dark__theme')
+    buttonChangeTheme.innerHTML = ''
     if (body.classList.contains('dark__theme')) {
-        buttonChangeTheme.insertAdjacentHTML('beforeend', '<use href="/symbol-defs.a8b2e413.svg#icon-sun"></use>');
+        buttonChangeTheme.insertAdjacentHTML('beforeend', '<use href="./images/symbol-defs.svg#icon-sun"></use>')
         // e.path[0].innerHTML = '<use href="/symbol-defs.a8b2e413.svg#icon-sun"></use>'
     } else {
-        buttonChangeTheme.insertAdjacentHTML('beforeend', '<use href="/symbol-defs.a8b2e413.svg#icon-moon"></use>');
+        buttonChangeTheme.insertAdjacentHTML('beforeend', '<use href="./images/symbol-defs.svg#icon-moon"></use>')
         // e.path[0].innerHTML = '<use href="/symbol-defs.a8b2e413.svg#icon-moon"></use>'
     }
-    
+
