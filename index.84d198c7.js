928e60c5b013498f031bcd15cfaae5ce74fc6a00
Alexf17 <98961066+Alexf17@users.noreply.github.com>
2022-09-17 12:40:20 +0000

    Deploying to gh-pages from @ Alexf17/Devs-ua-training@8f0ef0c03a994834e785253124ebd10d245d382f 🚀

---
+++ index.84d198c7.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".page");document.querySelector(".theme-switcher").addEventListener("click",(function(){e.classList.toggle("light__theme"),e.classList.toggle("dark__theme")}))}();
+//# sourceMappingURL=index.84d198c7.js.map
---
+++ index.8da6bbc9.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".page");document.querySelector(".theme-switcher").addEventListener("click",(function(){e.classList.toggle("light__theme"),e.classList.toggle("dark__theme")}));
+//# sourceMappingURL=index.8da6bbc9.js.map
