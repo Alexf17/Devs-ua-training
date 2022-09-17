646eab30ee2abace518324d8ce87954f73750dba
Oleksii Chilibiyskiy <98961066+Alexf17@users.noreply.github.com>
2022-09-17 18:26:57 +0300

    Update changeTheme.js

--- src/js/changeTheme.js
+++ src/js/changeTheme.js
@@ -8,7 +8,7 @@ function changeTheme(e) {
     page.classList.toggle('dark__theme')
     buttonChangeTheme.innerHTML = ''
     if (body.classList.contains('dark__theme')) {
-        buttonChangeTheme.insertAdjacentHTML('beforeend', '<use href="./images/symbol-defs.svg#icon-sun"></use>')
+        buttonChangeTheme.insertAdjacentHTML('beforeend', '<use href="/Devs-ua-training/symbol-defs.df4646d6.svg#icon-sun"></use>')
         // e.path[0].innerHTML = '<use href="/symbol-defs.a8b2e413.svg#icon-sun"></use>'
     } else {
         buttonChangeTheme.insertAdjacentHTML('beforeend', '<use href="./images/symbol-defs.svg#icon-moon"></use>')
