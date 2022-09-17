677584adb1e872adf7d80fa9edd2435e8d4f725e
Oleksii Chilibiyskiy <98961066+Alexf17@users.noreply.github.com>
2022-09-17 18:30:18 +0300

    Update changeTheme.js

--- src/js/changeTheme.js
+++ src/js/changeTheme.js
@@ -11,7 +11,7 @@ function changeTheme(e) {
         buttonChangeTheme.insertAdjacentHTML('beforeend', '<use href="/Devs-ua-training/symbol-defs.df4646d6.svg#icon-sun"></use>')
         // e.path[0].innerHTML = '<use href="/symbol-defs.a8b2e413.svg#icon-sun"></use>'
     } else {
-        buttonChangeTheme.insertAdjacentHTML('beforeend', '<use href="./images/symbol-defs.svg#icon-moon"></use>')
+        buttonChangeTheme.insertAdjacentHTML('beforeend', '<use href="/Devs-ua-training/symbol-defs.df4646d6.svg#icon-moon"></use>')
         // e.path[0].innerHTML = '<use href="/symbol-defs.a8b2e413.svg#icon-moon"></use>'
     }
 
