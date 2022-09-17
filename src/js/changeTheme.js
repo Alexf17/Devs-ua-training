f92d29b7ed7a2c269e1bc4769a0e81236e910c60
Oleksii <achili3f17@gmail.com>
2022-09-17 18:06:43 +0300

    333

--- src/js/changeTheme.js
+++ src/js/changeTheme.js
@@ -1,4 +1,5 @@
 const page = document.querySelector('.page')
+<<<<<<< Updated upstream
 const buttonChangeTheme = document.querySelector('.theme-switcher')
 
 buttonChangeTheme.addEventListener('click', changeTheme)
@@ -6,3 +7,21 @@ function changeTheme() {
     page.classList.toggle('light__theme')
     page.classList.toggle('dark__theme')
 }
+=======
+const buttonChangeTheme = document.querySelector('.switcher__svg')
+const body = document.querySelector('body')
+
+buttonChangeTheme.addEventListener('click', changeTheme)
+function changeTheme(e) {
+    
+    page.classList.toggle('dark__theme');
+    buttonChangeTheme.innerHTML = '';
+    if (body.classList.contains('dark__theme')) {
+        buttonChangeTheme.insertAdjacentHTML('beforeend', '<use href="/symbol-defs.a8b2e413.svg#icon-sun"></use>');
+        // e.path[0].innerHTML = '<use href="/symbol-defs.a8b2e413.svg#icon-sun"></use>'
+    } else {
+        buttonChangeTheme.insertAdjacentHTML('beforeend', '<use href="/symbol-defs.a8b2e413.svg#icon-moon"></use>');
+        // e.path[0].innerHTML = '<use href="/symbol-defs.a8b2e413.svg#icon-moon"></use>'
+    }
+    
+>>>>>>> Stashed changes
