3efb4402fbb033213994c35e3cbcad66e83e3dde
Alexf17 <98961066+Alexf17@users.noreply.github.com>
2022-09-18 14:27:32 +0000

    Deploying to gh-pages from @ Alexf17/Devs-ua-training@ddda03fad5a1457efdd649cb65638413692c9817 🚀

--- index.49c61403.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".page"),n=document.querySelector(".switcher__svg"),s=document.querySelector("body");n.addEventListener("click",(function(t){e.classList.toggle("dark__theme"),n.innerHTML="",s.classList.contains("dark__theme")?n.insertAdjacentHTML("beforeend",'<use href="/Devs-ua-training/symbol-defs.df4646d6.svg#icon-sun"></use>'):n.insertAdjacentHTML("beforeend",'<use href="/Devs-ua-training/symbol-defs.df4646d6.svg#icon-moon"></use>')}));
-//# sourceMappingURL=index.49c61403.js.map
