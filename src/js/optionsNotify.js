c928a998ece8ff433a8e80dbd1ae6ad87e39d076
AlexUKDev <61418792+AlexUKDev@users.noreply.github.com>
2022-09-19 15:21:28 +0300

    addsSomeFiches
    1. Перенес функцию клинер маркапа в отдельный файл для удобства пользования
    2. Перенес функцию создания Ли елементов в отдельную функцию и файл;
    3. Добавил проверку на пустую строку в инпуте, что бы не было запроса без введенных;
    4. Добавил обьект настроек в отдельный файл для библиотеки нотификашек, для удобства пользования (теперь достаточно зайти в одно место для кастомизации уведомлений);

---
+++ src/js/optionsNotify.js
@@ -0,0 +1,10 @@
+export const optionsNotify = {
+  width: "350px",
+  fontSize: "18px",
+  timeout: "3000",
+  distance: "15px",
+  cssAnimationDuration: "700",
+  borderRadius: "0px",
+  fontAwesomeIconStyle: "shadow",
+  cssAnimationStyle: "zoom",
+};
--- src/js/searchByName.js
+++ src/js/searchByName.js
@@ -1,6 +1,10 @@
 import { refs } from './refs';
 import ApiCoctails from './api';
 import { baseMarkUpCreate } from "./createMarkUp";
+import { Notify } from 'notiflix';
+import { optionsNotify } from './optionsNotify';
+import { cleanerMarkup } from './createMarkUp';
+
 const cocktailService = new ApiCoctails();
 
 
@@ -11,25 +15,28 @@ const cocktailService = new ApiCoctails();
 refs.formEl.addEventListener('submit', onSubmit)
 
 async function onSubmit(e) {
-    e.preventDefault();
-  cocktailService.name = e.currentTarget.elements.query.value
-    .trim()
-    .toLowerCase();
+  e.preventDefault();
+  // Add trim() mуthod
+  cocktailService.name = e.currentTarget.elements.query.value.trim().toLowerCase().trim();
+  
+  // Add check value for non-empty string
+  if (!cocktailService.name) {
+    Notify.info("You need to enter something Bro", optionsNotify);
+    return
+  }
+
     try {
         const { data } = await cocktailService.fetchByName();
         const drinks = data.drinks;
-        cleanerMarkup();
+        cleanerMarkup(refs.menu_container);
         baseMarkUpCreate(drinks, refs.menu_container);
         
     } catch (error) {
-        console.error(error)
-        cleanerMarkup()
+      console.error(error);
+      cleanerMarkup(refs.menu_container);
     }
   
 
   e.target.reset();
 }
 
-function cleanerMarkup() {
-  refs.menu_container.innerHTML = '';
-}
