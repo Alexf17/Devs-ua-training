0dda12d617c63a00237fbd66ad47f4fe83bee334
Darymee <danieledaniele3005@gmail.com>
2022-09-24 22:58:00 +0200

    add modalFilm

--- src/js/modal-filmoteka.js
+++ src/js/modal-filmoteka.js
@@ -1,35 +1,42 @@
-import {refs} from './refs'
+import { refs } from './refs';
 import MovieService from './film-api';
 import { genresList } from './genres-list';
 import { title } from 'process';
 
 const movieService = new MovieService();
 
-
 refs.filmList.addEventListener('click', onFilmItemClick);
+refs.closeBtnEl.addEventListener('click', onCloseFilmModal);
 
+console.log(refs.modalFilmWrap);
 
-function onFilmItemClick(event) {
-    event.preventDefault();
-    if (event.target.parentNode.parentElement.nodeName !== 'A') {
-        return
-    }
-    let filmCardId = event.target.parentNode.parentElement.id;
-    console.dir(event.target.parentNode.parentElement);
-    movieService.filmId = filmCardId;
-    const backendData = movieService.fetchById();
-   
+async function onFilmItemClick(event) {
+  event.preventDefault();
+  if (event.target.parentNode.parentElement.nodeName !== 'A') {
+    return;
   }
+  let filmCardId = event.target.parentNode.parentElement.id;
+  console.dir(event.target.parentNode.parentElement);
+  movieService.filmId = filmCardId;
+  const backendData = await movieService.fetchById();
+  const markup = await createFilmModalMarkup(backendData.data);
+  renderFilmModalMarkup(markup);
+  refs.backdropEl.classList.remove('visually-hidden');
+}
 
-  function createFilmModalMarkup(data) {
-const markup = data(({poster_path,
+function createFilmModalMarkup(data) {
+  const {
+    poster_path,
     original_title,
     title,
     vote_average,
     vote_count,
     genres,
     popularity,
-    overview,}) => `<img class="modal-film-image" src="https://image.tmdb.org/t/p/original${poster_path}
+    overview,
+  } = data;
+  const genreItems = genres.map(({ name }) => name).join(', ');
+  const markup = `<img class="modal-film-image" src="https://image.tmdb.org/t/p/original${poster_path}
     " alt="${title}">
     <h2 class="modal-film-title">${title}</h2>
     <ul class="modal-film-properties-list">
@@ -42,14 +49,21 @@ const markup = data(({poster_path,
       <li class="modal-film-value-item"><span class="modal-film-vote-average">${vote_average}</span>/<span class="modal-film-vote-count">${vote_count}</span></li>
       <li class="modal-film-value-item">${popularity}</li>
       <li class="modal-film-value-item">${original_title}</li>
-      <li class="modal-film-value-item">${genres.name.join(', ')}</li>
+      <li class="modal-film-value-item">${genreItems}</li>
     </ul>
     <h3 class="modal-film-about-title">ABOUT</h3>
     <p class="modal-film-about-text">${overview}</p>
     <ul class="modal-film-button-list">
       <li class="modal-film-button-item"><button class="modal-film-button" type="button">ADD TO WATCHED</button></li>
       <li class="modal-film-button-item"><button class="modal-film-button" type="button">ADD TO QUEUE</button></li>
-    </ul>`) 
+    </ul>`;
+  return markup;
 }
-  
 
+function renderFilmModalMarkup(markup) {
+  refs.modalFilmWrap.insertAdjacentHTML('beforeend', markup);
+}
+
+function onCloseFilmModal() {
+  refs.backdropEl.classList.add('visually-hidden');
+}
